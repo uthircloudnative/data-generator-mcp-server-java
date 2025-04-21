3e558ebe0465345b24d80c5de3afff0f7c664b73
uthircloudnative <uthircloudnative@gmail.com>
2025-04-21 00:39:18 -0500

    first commit

---
+++ src/main/java/com/learntech/mcp/DataGeneratorJavaMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.learntech.mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataGeneratorJavaMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataGeneratorJavaMcpServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learntech/mcp/DataGeneratorJavaMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learntech.mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataGeneratorJavaMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
